c285f88939f4554cb1eb11f87589dcc8b519a446
troshkin sergey <legat.psu@gmail.com>
2018-05-19 02:01:29 +0500

    convert legacy addresses to bcash format in insight api calls

--- src/Lykke.Service.BitcoinCash.API.Services/BlockChainProviders/InsightApi/InsightApiBlockChainProvider.cs
+++ src/Lykke.Service.BitcoinCash.API.Services/BlockChainProviders/InsightApi/InsightApiBlockChainProvider.cs
@@ -6,21 +6,27 @@ using System.Threading.Tasks;
 using Common.Log;
 using Flurl;
 using Flurl.Http;
+using Lykke.Service.BitcoinCash.API.Core.Address;
 using Lykke.Service.BitcoinCash.API.Core.BlockChainReaders;
 using Lykke.Service.BitcoinCash.API.Core.Domain.Health.Exceptions;
 using Lykke.Service.BitcoinCash.API.Services.BlockChainProviders.InsightApi.Contracts;
 using NBitcoin;
+using NBitcoin.Altcoins;
 
 namespace Lykke.Service.BitcoinCash.API.Services.BlockChainProviders.InsightApi
 {
     internal class InsightApiBlockChainProvider : IBlockChainProvider
     {
         private readonly InsightApiSettings _insightApiSettings;
+        private readonly IAddressValidator _addressValidator;
+        private readonly Network _network;
         private readonly ILog _log;
 
-        public InsightApiBlockChainProvider(InsightApiSettings insightApiSettings, ILog log)
+        public InsightApiBlockChainProvider(InsightApiSettings insightApiSettings, IAddressValidator addressValidator, Network network, ILog log)
         {
             _insightApiSettings = insightApiSettings;
+            _addressValidator = addressValidator;
+            _network = network;
             _log = log;
         }
 
@@ -59,12 +65,16 @@ namespace Lykke.Service.BitcoinCash.API.Services.BlockChainProviders.InsightApi
             var unspentOutputs = (await GetUnspentOutputsResponce(address)).Where(p => p.Confirmation >= minConfirmationCount);
 
             return unspentOutputs.Sum(p => p.Satoshi);
-        }      
+        }
 
         private async Task<IEnumerable<AddressUnspentOutputsResponce>> GetUnspentOutputsResponce(string address)
         {
-            var url = _insightApiSettings.Url
-                .AppendPathSegment($"api/addr/{address}/utxo");
+            if (_network == Network.Main)
+            {
+                var parsedAddress = _addressValidator.GetBitcoinAddress(address);
+                address = parsedAddress.ScriptPubKey.GetDestinationAddress(BCash.Instance.Mainnet).ToString();
+            }
+            var url = _insightApiSettings.Url.AppendPathSegment($"api/addr/{address}/utxo");
 
             return await GetJson<AddressUnspentOutputsResponce[]>(url);
         }
@@ -95,15 +105,15 @@ namespace Lykke.Service.BitcoinCash.API.Services.BlockChainProviders.InsightApi
         {
             bool NeedToRetryException(Exception ex)
             {
-                if (!(ex is FlurlHttpException flurlException))                
-                    return false;                
-                
-                if (flurlException is FlurlHttpTimeoutException)                
-                    return true;               
+                if (!(ex is FlurlHttpException flurlException))
+                    return false;
+
+                if (flurlException is FlurlHttpTimeoutException)
+                    return true;
 
                 if (flurlException.Call.HttpStatus == HttpStatusCode.ServiceUnavailable ||
-                    flurlException.Call.HttpStatus == HttpStatusCode.InternalServerError)                
-                    return true;                
+                    flurlException.Call.HttpStatus == HttpStatusCode.InternalServerError)
+                    return true;
                 return false;
             }
 
