a064ad8225c215d33207b5f62d4c3b46047ac680
Roman Chuprikov <paradise.develop@gmail.com>
2018-05-19 02:21:00 +0500

    LWDEV-7523: add address conversion

--- tests/Lykke.Service.BitcoinCash.API.Tests/AddressValidatorTests.cs
+++ tests/Lykke.Service.BitcoinCash.API.Tests/AddressValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using Lykke.Service.BitcoinCash.API.Services.Address;
+using NBitcoin.Altcoins;
 using Xunit;
 
 namespace Lykke.Service.BitcoinCash.API.Tests
@@ -33,6 +34,17 @@ namespace Lykke.Service.BitcoinCash.API.Tests
             var addressValidator = new AddressValidator();
 
             Assert.False(addressValidator.IsValid(invalidAddress));
+        }
+
+        [Fact]
+        public void CheckCashAddress()
+        {
+            var address = "17hFbeAibPyJ36PeH4hFDsjr35YC7wWmhv";
+            var addressValidator = new AddressValidator();
+            var parsedAddress = addressValidator.GetBitcoinAddress(address);
+            var result = parsedAddress.ScriptPubKey.GetDestinationAddress(BCash.Instance.Mainnet).ToString();
+
+            Assert.Equal("bitcoincash:qpykm9qwke5zj8je0sqqwu2tde6ej7lsvu2z4swqqj", result);
         }
     }
 
