423ed40e70fde30c1875f2bd125e7c3957143d7e
some-dev <avolkov.dev@gmail.com>
2018-12-07 13:00:34 +0300

    small refactoring

--- src/Lykke.Service.BitcoinCash.API.Services/BlockChainProviders/InsightApi/RpcBlockchainProvider.cs
+++ src/Lykke.Service.BitcoinCash.API.Services/BlockChainProviders/InsightApi/RpcBlockchainProvider.cs
@@ -47,12 +47,26 @@ namespace Lykke.Service.BitcoinCash.API.Services.BlockChainProviders.InsightApi
 
         public Task ImportWatchOnlyAddress(string address)
         {
-            return _client.ImportAddressAsync(_addressValidator.GetBitcoinAddress(address));
+            var btcAddr = _addressValidator.GetBitcoinAddress(address);
+
+            if (btcAddr == null)
+            {
+                throw new ArgumentException("Unable to recognize address", nameof(address));
+            }
+
+            return _client.ImportAddressAsync(btcAddr);
         }
 
         public async Task<IList<Coin>> GetUnspentOutputs(string address, int minConfirmationCount)
         {
-            var rpcResponce = await _client.ListUnspentAsync(minConfirmationCount, int.MaxValue, _addressValidator.GetBitcoinAddress(address));
+            var btcAddr = _addressValidator.GetBitcoinAddress(address);
+
+            if (btcAddr == null)
+            {
+                throw new ArgumentException("Unable to recognize address", nameof(address));
+            }
+
+            var rpcResponce = await _client.ListUnspentAsync(minConfirmationCount, int.MaxValue, btcAddr);
 
             return rpcResponce.Select(p => new Coin(p.OutPoint, new TxOut(p.Amount, p.ScriptPubKey))).ToList();
         }
